87c9a7599429e5a7567767e683987b13ad5c9277
kevinchenghua <s9822108@m98.nthu.edu.tw>
2015-12-24 14:24:02 +0800

    developing

--- src/RunTest.scala
+++ src/RunTest.scala
@@ -1,5 +1,23 @@
-
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd.RDD
 
 object RunTest {
-  def main
+  def main(args: Array[String]): Unit = {
+    //// Set parameters
+    // # of images
+    val m = 400
+    // # of rows/cols of each images
+    val p = 64 
+    val q = 64 
+    // # of singular values to pick
+    val r = 50
+    // # of rows/cols to pick in folding version
+    val a = 10
+    val b = 10
+    // # of random times in random version
+    val n_rand = 100
+    println("test!!!!!!!!!!!")
+  }
 }
